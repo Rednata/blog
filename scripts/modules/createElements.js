8e35e489a3456712c9dc9cb858d56800c6cf4043
Rednata <maknatosh@gmail.com>
2022-12-01 11:50:26 +0700

    modules for js

--- scripts/modules/createElements.js
+++ scripts/modules/createElements.js
@@ -1,3 +1,5 @@
+import { loadAuthor } from './fetchLoad.js';
+
 const createUl = () => {  
   const ul = document.createElement('ul');
   ul.classList.add('blog');
@@ -37,95 +39,96 @@ const createElementsBlog = ({ title, body, id }) => {
 
 const createBlog = (posts) => {
   const ul = createUl();
-
   const data = posts.map(elem => createElementsBlog(elem));
-  ul.append(...data);
+  ul.append(...data);  
+  const section = document.querySelector('section');  
+  section.prepend(ul);  
 
   // =============  ИЛИ ==================
   // posts.forEach(element => {
   //   ul.append(createElementsBlog(element));    
   // });
-  
-  const section = document.querySelector('section');  
-  section.prepend(ul);  
-
 }
 
-// const createElementsArticle = (articleData) => {  
-//   const {
-//     data: {title},
-//     data: {body},
-//     data: {user_id}
-//   } = articleData;
-  
-// const bodyArticle = document.querySelector('body');
-// const header = document.createElement('header');
-// header.classList.add('header');
-// header.insertAdjacentHTML('afterbegin', 
-// `
-//   <ul class="nav">
-//     <li class="nav__li">
-//       <a class="nav__main">Главная</a>
-//     </li>
-//     <li class="nav__li">
-//       <a href="blog.html">Блог</a>
-//     </li>
-//     <li class="nav__title">${title}
-//     </li>      
-//   </ul>  
-// `);
+//  ============== ARTICLE =======================
 
-// const main = document.createElement('main');
-// main.classList.add('main');
+const bodyArticle = document.querySelector('body');
 
-// main.insertAdjacentHTML('afterbegin', 
-// `
-//   <article class="article">
-//     <h1 class="title article__title">${title}</h1>
-//     <p class="article__text">${body}</p>
-//   </article>
-// `
-// );
+const createHeader = (title) => {
+  const header = document.createElement('header');
+  header.classList.add('header');
+  header.insertAdjacentHTML('afterbegin', 
+  `
+    <ul class="nav">
+      <li class="nav__li">
+        <a class="nav__main">Главная</a>
+      </li>
+      <li class="nav__li">
+        <a href="blog.html">Блог</a>
+      </li>
+      <li class="nav__title">${title}
+      </li>      
+    </ul>  
+  `);
+  return header;
+}
 
-// const loadAuthor = async (createFooter) => {
-//   console.log(user_id);
-//   const response = await fetch(`https://gorest.co.in/public-api/users/${user_id}`);
-//   const author = await response.json();
-//   createFooter(author.data.name);
-// }
+const createMain = (title, body) => {
+  const main = document.createElement('main');
+  main.classList.add('main');  
+  main.insertAdjacentHTML('afterbegin', 
+  `
+    <article class="article">
+      <h1 class="title article__title">${title}</h1>
+      <p class="article__text">${body}</p>
+    </article>
+  `
+  );
+  return main
+}
 
-// const createFooter = (author) => {
-//   const footer = document.createElement('footer');
-//   footer.classList.add('footer');
-//   footer.insertAdjacentHTML('afterbegin', 
-//   `
-//     <a class="footer__back" href="blog.html">К списку статей</a>
-//       <div class="footer__about">
-//       <div class="footer__author">
-//         ${author}
-//       </div>
-//       <div class="footer__date">
-//         22 октября 2021, 12:45          
-//       </div>
-//       <div class="info footer__info">
-//         <div class="info__view">
-//         <span class="info__img info__img_view"></span>
-//         <p class="info__view_count">1.2K</p>              
-//       </div>
-//       <div class="info__comments">
-//         <span class="info__img info__img_comments"></span>
-//         <p class="info__comments_count">0</p>              
-//       </div>
-//       </div>
-//     </div>
-//   `
-//   )
+const createFooter = (author) => {
+  const footer = document.createElement('footer');
+  footer.classList.add('footer');
+  footer.insertAdjacentHTML('afterbegin', 
+  `
+    <a class="footer__back" href="blog.html">К списку статей</a>
+      <div class="footer__about">
+      <div class="footer__author">
+        ${author}
+      </div>
+      <div class="footer__date">
+        22 октября 2021, 12:45          
+      </div>
+      <div class="info footer__info">
+        <div class="info__view">
+        <span class="info__img info__img_view"></span>
+        <p class="info__view_count">1.2K</p>              
+      </div>
+      <div class="info__comments">
+        <span class="info__img info__img_comments"></span>
+        <p class="info__comments_count">0</p>              
+      </div>
+      </div>
+    </div>
+  `
+  )
+  bodyArticle.append(footer);
+};
 
-//   bodyArticle.append(footer);
-// };
+const createElementsArticle = (articleData) => {  
+  const {
+    data: {title},
+    data: {body},
+    data: {user_id}
+  } = articleData;  
 
-//   loadAuthor(createFooter);
-//   bodyArticle.append(header, main);
-// }
+  const header = createHeader(title);
+  const main = createMain(title, body);
 
-export {createBlog, createElementsBlog};
+  loadAuthor(user_id);
+ 
+  bodyArticle.append(header, main);
+}
+
+export {createBlog, createElementsBlog, createElementsArticle, createFooter};
--- scripts/modules/fetchLoad.js
+++ scripts/modules/fetchLoad.js
@@ -1,4 +1,4 @@
-import { createBlog, createElementsBlog } from './createElements.js';
+import { createBlog, createElementsArticle, createFooter } from './createElements.js';
 
 const loadPosts = async () => {  
   const response = await fetch ('https://gorest.co.in/public-api/posts');
@@ -6,7 +6,25 @@ const loadPosts = async () => {
   createBlog(posts);  
 }
 
+const getURLSearch = () => {
+  return window.location.search.slice(4);
+}
 
+const onLoadWindow = () => {
+  window.addEventListener('load', () => {
+    const urlSearch = getURLSearch()  
+    const loadArticle = async (urlSearch) => {
+      const response = await fetch (`https://gorest.co.in/public-api/posts/${urlSearch}`);
+      createElementsArticle(await response.json());      
+    }
+    loadArticle(urlSearch);  
+  });
+}
 
+const loadAuthor = async (user_id) => {  
+  const response = await fetch(`https://gorest.co.in/public-api/users/${user_id}`);
+  const author = await response.json();
+  createFooter(author.data.name);
+}
 
-export {loadPosts};
+export {loadPosts, onLoadWindow, loadAuthor};
